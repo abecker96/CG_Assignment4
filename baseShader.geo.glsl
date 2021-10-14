a451e4f249e15cdb4bccf50d9e5c299a7908a7a4
Aidan Becker <abecker2@islander.tamucc.edu>
2021-10-14 18:55:02 -0500

    MVP multiplication shifted from vertex shader to geometry shader

--- baseShader.geo.glsl
+++ baseShader.geo.glsl
@@ -3,40 +3,49 @@
 // mainly learned from https://learnopengl.com/Advanced-OpenGL/Geometry-Shader
 
 layout(triangles) in;
-layout(triangle_strip, max_vertices=3) out;
+layout(triangle_strip, max_vertices=6) out;
 
 in vec3 vPosition_modelspace0[];
 uniform float geoTimer;
+//Arranged in this order:
+//scaling, rotation, translation, view, projection matrices
+uniform mat4 matrices[5];
 
 out vec3 vPosition_modelspace1;
+out vec3 vNormal;
 
 vec3 getNormal()
 {
-    vec3 a = vec3(gl_in[0].gl_Position) - vec3(gl_in[1].gl_Position);
-    vec3 b = vec3(gl_in[2].gl_Position) - vec3(gl_in[1].gl_Position);
-    return normalize(cross(a, b));
-}
+   vec3 a = vec3(gl_in[0].gl_Position) - vec3(gl_in[1].gl_Position);
+   vec3 b = vec3(gl_in[2].gl_Position) - vec3(gl_in[1].gl_Position);
+   return normalize(cross(b, a));
+}  
 
 vec4 explode(vec4 position, vec3 normal)
 {
-    float magnitude = 0.10;
-    vec3 direction = normal * ((sin(geoTimer) + 1.0) / 2.0) * magnitude; 
+    vec3 direction = normal * (sin(geoTimer)+1) * 0.01; 
     return position + vec4(direction, 0.0);
-} 
+}
 
 void main() {
     vec3 normal_face = getNormal();
+    //link vertexPos with gl_Position
+    //Generate MVP matrix
+    mat4 MVP = matrices[4] * matrices[3] * matrices[2] * matrices[1] * matrices[0];
 
-    gl_Position = explode(gl_in[0].gl_Position, normal_face);
+    gl_Position = MVP * explode(gl_in[0].gl_Position, normal_face);
     vPosition_modelspace1 = vPosition_modelspace0[0];
+    vNormal = normal_face;
     EmitVertex();
 
-    gl_Position = explode(gl_in[1].gl_Position, normal_face);
+    gl_Position = MVP * explode(gl_in[1].gl_Position, normal_face);
     vPosition_modelspace1 = vPosition_modelspace0[1];
+    vNormal = normal_face;
     EmitVertex();
 
-    gl_Position = explode(gl_in[2].gl_Position, normal_face);
+    gl_Position = MVP * explode(gl_in[2].gl_Position, normal_face);
     vPosition_modelspace1 = vPosition_modelspace0[2];
+    vNormal = normal_face;
     EmitVertex();
 
     EndPrimitive();
--- baseShader.vrt.glsl
+++ baseShader.vrt.glsl
@@ -6,17 +6,11 @@
 // takes a vec3, vPosition_Modelspace is just a name that makes sense
 layout(location = 0) in vec3 vPosition_Modelspace;
 // layout(location = 1) in vec3 vertexColor;
-//Arranged in this order:
-//scaling, rotation, translation, view, projection matrices
-uniform mat4 matrices[5];
 
 out vec3 vPosition_modelspace0;
 
 void main() {
-    //link vertexPos with gl_Position
-    //Generate MVP matrix
-    mat4 MVP = matrices[4] * matrices[3] * matrices[2] * matrices[1] * matrices[0];
-    gl_Position = MVP * vec4(vPosition_Modelspace, 1.0);
+    gl_Position = vec4(vPosition_Modelspace, 1.0);
 
     //forward color data on to fragment shader
     // fragmentColor = vertexColor;
