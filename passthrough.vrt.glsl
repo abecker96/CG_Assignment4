b117e50791ed8298da340dd5578392d805a70c7a
Aidan Becker <abecker2@islander.tamucc.edu>
2021-10-15 09:00:13 -0500

    Vertex shader now more accurately a passthrough

--- passthrough.vrt.glsl
+++ passthrough.vrt.glsl
@@ -5,14 +5,14 @@
 // "in" notes that this is input data
 // takes a vec3, vPosition_Modelspace is just a name that makes sense
 layout(location = 0) in vec3 vPosition_Modelspace;
-// layout(location = 1) in vec3 vertexColor;
+layout(location = 1) in vec3 vertexColor;
 
-out vec3 vPosition_modelspace0;
+out vec3 fragColor0;
 
 void main() {
+    // link vertex position with passed data
     gl_Position = vec4(vPosition_Modelspace, 1.0);
 
     //forward color data on to fragment shader
-    // fragmentColor = vertexColor;
-    vPosition_modelspace0 = vPosition_Modelspace;
+    fragColor0 = vertexColor;
 }
