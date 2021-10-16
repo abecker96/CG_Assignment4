a5a673ee7cdf5140ee375f9ec5a478e9739a7069
Aidan Becker <abecker2@islander.tamucc.edu>
2021-10-16 11:38:01 -0500

    Ability to toggles user input

--- AidanGLCamera.h
+++ AidanGLCamera.h
@@ -17,7 +17,7 @@ class Camera {
     public:
         Camera()
         {}
-        void init(GLFWwindow *cameraWindow, glm::vec3 pos, glm::mat4 perspective, float horizontalAngle, float verticalAngle, float speed, float sens)
+        void init(GLFWwindow *cameraWindow, glm::vec3 pos, glm::mat4 perspective, float horizontalAngle, float verticalAngle, float speed, float sens, bool inputType)
         {
             // Initialize basic data
             window = cameraWindow;
@@ -28,6 +28,7 @@ class Camera {
             perspectiveMatrix = perspective;
             cameraSpeed = speed;
             mouseSensitivity = sens;
+            allowInput = inputType;
 
             lastTime = glfwGetTime();
             update();
@@ -38,59 +39,61 @@ class Camera {
             currentTime = glfwGetTime();
             deltaTime = currentTime - lastTime;
 
-            // Get mouse position
-            double mouseX, mouseY;
-            glfwGetCursorPos(window, &mouseX, &mouseY);
-            // Need to reset cursor to a known location
-            glfwSetCursorPos(window, windowSizeX/2, windowSizeY/2);
+            if(allowInput)
+            {
+                // Get mouse position
+                double mouseX, mouseY;
+                glfwGetCursorPos(window, &mouseX, &mouseY);
+                // Need to reset cursor to a known location
+                glfwSetCursorPos(window, windowSizeX/2, windowSizeY/2);
 
-            // Calculate viewing angles based on mouse movement
-            angleX += mouseSensitivity * (float)deltaTime * (windowSizeX/2 - (float)mouseX);
-            angleY += mouseSensitivity * (float)deltaTime * (windowSizeY/2 - (float)mouseY);
+                // Calculate viewing angles based on mouse movement
+                angleX += mouseSensitivity * (float)deltaTime * (windowSizeX/2 - (float)mouseX);
+                angleY += mouseSensitivity * (float)deltaTime * (windowSizeY/2 - (float)mouseY);
 
-            updateCameraDirection();        // update direction vector
-            updateCameraRight();            // update vector facing to the right of the camera, to calculate the up vector
-            updateCameraUp();               // update the up vector
-            updateViewMatrix();             // calculate viewmatrix
+                updateCameraDirection();        // update direction vector
+                updateCameraRight();            // update vector facing to the right of the camera, to calculate the up vector
+                updateCameraUp();               // update the up vector
+                updateViewMatrix();             // calculate viewmatrix
 
-            // Check keyboard input for camera movement
-            int wKeyState = glfwGetKey(window, GLFW_KEY_W);
-            int aKeyState = glfwGetKey(window, GLFW_KEY_A);
-            int sKeyState = glfwGetKey(window, GLFW_KEY_S);
-            int dKeyState = glfwGetKey(window, GLFW_KEY_D);
-            int spaceKeyState = glfwGetKey(window, GLFW_KEY_SPACE);
-            int shiftKeyState = glfwGetKey(window, GLFW_KEY_LEFT_SHIFT);
-            // Move forward
-            if(wKeyState == GLFW_PRESS)
-            {
-                position += direction * (float)deltaTime * cameraSpeed;
+                // Check keyboard input for camera movement
+                int upKeyState = glfwGetKey(window, GLFW_KEY_W);
+                int leftKeyState = glfwGetKey(window, GLFW_KEY_A);
+                int downKeyState = glfwGetKey(window, GLFW_KEY_S);
+                int rightKeyState = glfwGetKey(window, GLFW_KEY_D);
+                int spaceKeyState = glfwGetKey(window, GLFW_KEY_SPACE);
+                int shiftKeyState = glfwGetKey(window, GLFW_KEY_LEFT_SHIFT);
+                // Move forward
+                if(upKeyState == GLFW_PRESS)
+                {
+                    position += direction * (float)deltaTime * cameraSpeed;
+                }
+                // Move backward
+                if(downKeyState == GLFW_PRESS)
+                {
+                    position -= direction * (float)deltaTime * cameraSpeed;
+                }
+                // Move right
+                if(rightKeyState == GLFW_PRESS)
+                {
+                    position += right * (float)deltaTime * cameraSpeed;
+                }
+                // Move left
+                if(leftKeyState == GLFW_PRESS)
+                {
+                    position -= right * (float)deltaTime * cameraSpeed;
+                }
+                // Move up
+                if(spaceKeyState == GLFW_PRESS)
+                {
+                    position += up * (float)deltaTime * cameraSpeed;
+                }
+                // Move down
+                if(shiftKeyState == GLFW_PRESS)
+                {
+                    position -= up * (float)deltaTime * cameraSpeed;
+                }
             }
-            // Move backward
-            if(sKeyState == GLFW_PRESS)
-            {
-                position -= direction * (float)deltaTime * cameraSpeed;
-            }
-            // Move right
-            if(dKeyState == GLFW_PRESS)
-            {
-                position += right * (float)deltaTime * cameraSpeed;
-            }
-            // Move left
-            if(aKeyState == GLFW_PRESS)
-            {
-                position -= right * (float)deltaTime * cameraSpeed;
-            }
-            // Move up
-            if(spaceKeyState == GLFW_PRESS)
-            {
-                position += up * (float)deltaTime * cameraSpeed;
-            }
-            // Move down
-            if(shiftKeyState == GLFW_PRESS)
-            {
-                position -= up * (float)deltaTime * cameraSpeed;
-            }
-
             // timing data
             lastTime = currentTime;
         }
@@ -114,6 +117,14 @@ class Camera {
         {
             return viewMatrix;
         }
+        void disableUserInput()
+        {
+            allowInput = false;
+        }
+        void enableUserInput()
+        {
+            allowInput = true;
+        }
     private:
         GLFWwindow *window;                                         // Camera needs to know what window it's rendering in
         glm::mat4 viewMatrix, perspectiveMatrix;
@@ -121,6 +132,7 @@ class Camera {
         int windowWidth, windowHeight, windowSizeX, windowSizeY;
         float cameraSpeed, mouseSensitivity, angleX, angleY;
         double lastTime, currentTime, deltaTime;
+        bool allowInput;
         
         glm::vec3 getCameraDirection()
         {
